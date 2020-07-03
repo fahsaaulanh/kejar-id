d58f70c88bba66da180fa2931d7d049976bf2040
iqbal ramadhan <iqbal.ramadhan3000@gmail.com>
2020-07-03 06:56:41 +0000

    15-user-matrikulasi-beranda

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Login</title>
+        <title>Siswa | Beranda</title>
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
         <link href="{{ url('/assets/css/layout.css') }}" rel="stylesheet">
@@ -55,5 +55,6 @@
             </div>
         </div>
         </div>
+
     </body>
 </html>
