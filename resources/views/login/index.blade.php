3c16a6dc80f7d73fc94049d69c7eb3775f135b6e
Mochamad Ardafa Pratama <ardafapratama123@gmail.com>
2020-07-03 03:58:19 +0000

    9-curriculum-admin-matrikulasi-halaman-login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,29 +3,33 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Login</title>
+        <title>Selamat Datang</title>
         <!-- Styles -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
         <link href="{{ url('/assets/css/layout.css') }}" rel="stylesheet">
         <link href="{{ url('/assets/css/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ url('/assets/css/login/style.css') }}" rel="stylesheet">
     </head>
-    <body class="default-body">
-        <div class="flex-center position-ref full-height">
-            <h4>This is Login Page.</h4>
-            @if ($message)
-                <p class="text-danger">{{ $message }}</p>
-            @endif
-            <form method="POST" id="loginForm" action="{{ url('/login/doLogin') }}">
-                @csrf
-                <div class="form-group mt-1">
-                    <Label for="username">Username</Label>
-                    <input class="form-control id="username" type="text" name="username" />
-                </div>
-                <div class="form-group mt-1">
-                    <Label for="password">Password</Label>
-                    <input class="form-control  id="password" type="password" name="password" />
-                </div>
-                <button class="btn btn-primary mt-1" id="submit" type="submit" class="mt-1">Login</button>
-            </form>
+    <body>
+        <div class="login-container">
+            <div class="login-header">
+                <h5>Kejar Matrikulasi</h5>
+            </div>
+            <div class="bg-mobile"></div>
+            <div class="login-body">
+                <form method="POST" id="loginForm" action="{{ url('/login/doLogin') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control" id="username" autocomplete="off" placeholder="Ketik username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" id="password" autocomplete="off" placeholder="Ketik password">
+                    </div>
+                    <button type="submit" class="btn btn-block btn-login">Masuk</button>
+                </form>
+            </div>
         </div>
     </body>
 </html>
