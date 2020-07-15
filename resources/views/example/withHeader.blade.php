cf8a4b179a692416e6634bd87cb48a8fb540e33a
Mutia Prawitasari <prawitamutia@gmail.com>
2020-07-15 05:18:40 +0000

    36 apply refactoring boilerplate

--- resources/views/example/withHeader.blade.php
+++ resources/views/example/withHeader.blade.php
@@ -4,6 +4,57 @@
 
 @section('content')
     <div class="container">
-        <h1>Ini content!</h1>
+        <!-- Link Back -->
+        <a class ="btn-back" href="">
+            <i class="kejar-back"></i>Kembali
+        </a>
+        <!-- Breadcrumb -->
+        <nav class="breadcrumb">
+            <a class="breadcrumb-item" href="">Beranda</a>
+            <a class="breadcrumb-item" href="">OBR</a>
+            <a class="breadcrumb-item" href="">Babak 1</a>
+            <span class="breadcrumb-item active">Ronde 1</span>
+        </nav>
+        <!-- Title -->
+        <div class="page-title">
+            <h2 class="mb-08rem">Ini Judul Babak atau Ronde</h2>
+            <span class="copy-id" onclick="">Salin ID Ronde</span>
+        </div>
+
+        <!-- Upload Buttons -->
+        <div class="upload-buttons">
+            <button class="btn-upload" data-toggle="modal" data-target="">
+                <i class="kejar-upload"></i>Unggah Babak
+            </button>
+            <button class="btn-upload" data-toggle="modal" data-target="">
+                <i class="kejar-upload"></i>Unggah Ronde
+            </button>
+        </div>
+
+        <!-- List of Stages (Admin)-->
+<<<<<<< HEAD
+        <div class="list-group">
+=======
+        <div class="list-group">       
+>>>>>>> add basic scss
+            <div class="list-group-item">
+                <a href="#">
+                    <i class="kejar-ink"></i>
+                    Ini Judul Babak dalam Permainan
+                </a>
+                <!-- <div class="hover-only"> -->
+                    <div class="stage-order-buttons">
+                        <button class="btn-icon">
+                            <i class="kejar-top"></i>
+                        </button>
+                        <button class="btn-icon">
+                            <i class="kejar-bottom"></i>
+                        </button>
+                    </div>
+                <!-- </div> -->
+            </div>
+        </div>
+
+
     </div>
 @endsection
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,12 +9,12 @@
 </head>
 <body>
     @section('header')
-        <nav class="navbar navbar-light bg-dark-custom align-items-center">
-            <a class="navbar-brand align-items-center" href="#">
-                <img src="{{ asset('assets/icons/kejarid.svg') }}" width="40" height="40" alt="" loading="lazy">
+        <nav class="navbar">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets/icons/kejarid.svg') }}" alt="" loading="lazy">
                 {{ session('user.username') }}
             </a>
-            <a href="{{ url('/logout') }}" class="btn-logout">Logout</a>
+            <a class="btn-logout" href="{{ url('/logout') }}">Logout</a>
         </nav>
     @show
 
