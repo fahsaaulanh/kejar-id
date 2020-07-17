78f79ced02b3fce34d0db7910df81a8e022e613d
Syukron Muttaqien <syukronmuttaqien74@gmail.com>
2020-07-17 18:44:35 +0700

    fix error

--- resources/views/admin/stages/index.blade.php
+++ resources/views/admin/stages/index.blade.php
@@ -6,13 +6,13 @@
     <div class="container">
 
         <!-- Link Back -->
-        <a class ="btn-back" href="{{ secure_url('/admin/games') }}">
+        <a class ="btn-back" href="{{ url('/admin/games') }}">
             <i class="kejar-back"></i>Kembali
         </a>
 
         <!-- Breadcrumb -->
         <nav class="breadcrumb">
-            <a class="breadcrumb-item" href="{{ secure_url('/admin/games') }}">Beranda</a>
+            <a class="breadcrumb-item" href="{{ url('/admin/games') }}">Beranda</a>
             <span class="breadcrumb-item active">{{ $game['short'] }}</span>
         </nav>
 
