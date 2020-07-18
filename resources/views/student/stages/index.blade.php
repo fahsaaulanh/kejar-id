c9ea8b8692b3416f1acf39f574fa9771be921d45
Muhamad Firmansyah <mfirmansyah.work@gmail.com>
2020-07-18 12:23:10 +0000

    50 perbaikan halaman siswa stages

--- resources/views/student/stages/index.blade.php
+++ resources/views/student/stages/index.blade.php
@@ -1,57 +1,46 @@
-@extends('layout.main')
+@extends('layout.index')
 
-@section('title', 'Student Stages')
-
-@section('css')
-<link rel="stylesheet" href="{{ url('/assets/css/bootstrap.css') }}">
-<link rel="stylesheet" href="{{ url('/assets/css/student_game.css') }}">
-@endsection
+@section('title', 'Babak Permainan')
 
 @section('content')
-
-        <!-- Content -->
-        <div class="container-fluid">
-            <div class="row justify-content-center content">
-                <div class="col-md-10 col-lg-6">
-
-                    <!-- Link back -->
-                    <div class="link-back">
-                        <a href="{{ url('/students/games') }}" class="text-link d-flex align-items-center">
-                            <i class="kejar kejar-arrow-left"></i> <span class="ml-2">Kembali</span>
-                        </a>
-                    </div>
-
-                    <!-- Breadcrumb -->
-                    <nav class="breadcrumb bg-transparent p-0">
-                        <a class="breadcrumb-item" href="{{ url('/students/games') }}">Beranda</a>
-                        <span class="breadcrumb-item active">{{ $game['uri'] }}</span>
-                    </nav>
-
-                    <!-- Title -->
-                    <h1 class="text-title">{{ $game['title'] }}</h1>
-
-                    <!-- Stages Group -->
-                    <div class="stages-group">
-                        <!-- Stage Item -->
-                        @foreach($stages as $key => $stage)
-                        <div class="stage-item">
-                            <div class="stage-icon">
-                                <i class="kejar kejar-right"></i>
-                            </div>
-                            <div class="stage-text w-100">
-                                <a href="{{ url('/students/games/' . $game['uri'] . '/stages/' . $stage['id'] . '/rounds') }}" class="text-link">
-                                    <span class="stage-number">Babak {{ ++$key }} : </span><span class="text-wrap">{{ $stage['title'] }}</span>
-                                </a>
-                            </div>
-                            <div class="stage-status-icon">
-                                @if($stage['status'] == 'done')
-                                <i class="kejar kejar-sudah-dikerjakan"></i>
-                                @endif
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
+<div class="container">
+        <!-- Link Back -->
+        <a class ="btn-back" href="{{ secure_url('students/games') }}">
+            <i class="kejar-back"></i>Kembali
+        </a>
+        <!-- Breadcrumb -->
+        <nav class="breadcrumb">
+            <a class="breadcrumb-item" href="{{ secure_url('students/games') }}">Beranda</a>
+            <span class="breadcrumb-item active">{{ $game['short'] }}</span>
+        </nav>
+        <!-- Title -->
+        <div class="page-title">
+            <h2 class="mb-08rem">Ini Judul Babak atau Ronde</h2>
         </div>
+
+        <!-- List of Stages (Student)-->
+        <div class="list-group list-group-student">
+            @forelse($stages as $stage)
+            <div class="list-group-item">
+                <a href="{{ secure_url('students/games/' . $game['uri'] . '/stages/' . $stage['id'] . '/rounds') }}">
+                    <i class="kejar-right"></i>
+                    <span>Babak {{ $loop->iteration }} : </span> <span>{{ $stage['title'] }}</span>
+                </a>
+                <!-- <div class="hover-only"> -->
+                    <div class="stage-order-buttons">
+                        @if($stage['status'] == 'done')
+                        <button class="btn-icon">
+                            <i class="kejar-sudah-dikerjakan"></i>
+                        </button>
+                        @endif
+                    </div>
+                <!-- </div> -->
+            </div>
+            @empty
+            <h5 class="text-center">Tidak ada data</h5>
+            @endforelse
+        </div>
+
+
+    </div>
 @endsection
