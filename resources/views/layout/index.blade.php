6fcfa9f75eb38d90906c977a45e853b30c750460
idsdeveloper <rifqizzz6@gmail.com>
2020-07-20 12:05:02 +0700

    add favicon

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="icon" href="{{ url('assets/icon/fackejar.png') }}" type="image/gif">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     @section('header')
