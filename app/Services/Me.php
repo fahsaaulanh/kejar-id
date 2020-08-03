0c98180ba11faaaa93ab1fca87ceb4b2a2244c5c
Mochamad Ardafa Pratama <ardafapratama123@gmail.com>
2020-08-03 13:29:37 +0700

    Adding File

--- app/Services/Me.php
+++ app/Services/Me.php
@@ -7,13 +7,6 @@ class Me extends Service
     public function update($payload)
     {
         $response = $this->patch('/me', $payload);
-        
-        return $this->showResponse($response);
-    }
-
-    public function me()
-    {
-        $response = $this->get('/me');
 
         return $this->showResponse($response);
     }
@@ -21,7 +14,7 @@ class Me extends Service
     public function me()
     {
         $response = $this->get('/me');
-        
+
         return $this->showResponse($response);
     }
 }
