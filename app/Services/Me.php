b2731e230021da47f44e7c2650736d94863733f5
Mochamad Ardafa Pratama <ardafapratama123@gmail.com>
2020-08-05 03:35:36 +0000

    76-user-siswa-header

--- app/Services/Me.php
+++ app/Services/Me.php
@@ -7,7 +7,14 @@ class Me extends Service
     public function update($payload)
     {
         $response = $this->patch('/me', $payload);
+        
+        return $this->showResponse($response);
+    }
 
+    public function me()
+    {
+        $response = $this->get('/me');
+        
         return $this->showResponse($response);
     }
 }
---
+++ resources/views/shared/_update_avatar.blade.php
@@ -0,0 +1,96 @@
+<!-- Modal -->
+<div class="modal fade" id="editProfile" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <form action="{{ url('student/change-profile') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @method('PATCH')
+                <div class="modal-header">
+                    <h5 class="modal-title">Foto Profil</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="kejar-close"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="avatar-group">
+                                @if (session('user.role') === 'STUDENT')
+                                    @if (!is_null(session('user.userable.photo')))
+                                    <img src="{{ session('user.userable.photo') }}" data-pict="notNull" class="profile-pict" alt="">
+                                    @else
+                                    <img src="{{ asset('assets/images/profile/default-picture.jpg') }}" data-pict="Null" class="profile-pict" alt="">
+                                    @endif
+                                @endif
+                                <button type="button" class="edit-pict-btn">
+                                    <i class="kejar-play"></i>
+                                </button>   
+                            </div>
+                            @if (session('user.role') === 'STUDENT')
+                                @if (!is_null(session('user.userable.photo')))
+                                <input type="hidden" name="photo" value="{{ session('user.userable.photo') }}">
+                                @else
+                                <input type="file" name="photo" hidden>
+                                @endif
+                            @endif
+                        </div>
+                        <div class="col-12">
+                            <h3>{{ session('user.userable.name') }}</h3>
+                            <table class="profile-table">
+                                <tr>
+                                    <td>NIS</td>
+                                    <td>{{ session('user.userable.nis') }}</td>
+                                </tr>
+                                <tr>
+                                    <td>NISN</td>
+                                    <td>{{ session('user.userable.nisn') }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Rombel</td>
+                                    <td>{{ session('user.userable.gender') }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Sekolah</td>
+                                    <td>{{ session('user.userable.gender') }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-end">
+                    <div>
+                        <button type="button" class="btn btn-cancel" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary save-btn-1">Simpan</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="updateProfile" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Atur Foto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <i class="kejar-close"></i>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="crop-area">
+                            <img src="{{ session('user.userable.photo') }}" class="profile-pict-crop" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-end">
+                <div>
+                    <button type="button" class="btn btn-cancel cancel-edit">Batal</button>
+                    <button type="button" class="btn btn-primary save-btn-2">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
