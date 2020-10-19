a624cb1cf1a375522bf5eb4f39c2ba62fcee4a48
idsdeveloper <rifqizzz6@gmail.com>
2020-10-20 06:00:19 +0700

    fix blob download

--- resources/views/student/mini_assessment/exam/index.blade.php
+++ resources/views/student/mini_assessment/exam/index.blade.php
@@ -242,31 +242,22 @@ return is_array($value['answer']);
                         type: 'application/pdf'
                     });
 
-                    // if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                    if (typeof window.navigator.msSaveBlob !== 'undefined') {
                         //   IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-                        // window.navigator.msSaveBlob(blob, filename);
-                    // } else {
-                        // var URL = window.URL || window.webkitURL;
-                        var downloadUrl = URL.createObjectURL(blob);
-
-                        // if (filename) {
-                        //     // use HTML5 a[download] attribute to specify filename
-                            var a = document.createElement("a");
-
-                        //     // safari doesn't support this yet
-                        //     if (typeof a.download === 'undefined') {
-                        //         window.location = downloadUrl;
-                        //     } else {
-                                a.href = downloadUrl;
-                                a.download = filename;
-                                document.body.appendChild(a);
-                                a.target = "_blank";
-                                a.click();
-                        //     }
-                        // } else {
-                        //     window.location = downloadUrl;
-                        // }
-                    // }
+                        window.navigator.msSaveBlob(blob, filename);
+                    } else {
+                        // For other browsers:
+                        // Create a link pointing to the ObjectURL containing the blob.
+                        const data = window.URL.createObjectURL(newBlob);
+                        var link = document.createElement('a');
+                        link.href = data;
+                        link.download="file.pdf";
+                        link.click();
+                        setTimeout(function(){
+                            // For Firefox it is necessary to delay revoking the ObjectURL
+                            window.URL.revokeObjectURL(data);
+                        }, 100);
+                    }
 
                 } catch (ex) {
                     console.log(ex);
