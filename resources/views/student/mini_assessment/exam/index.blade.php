85b696d096f2b1e92405fd01bddeb215d6d30a89
idsdeveloper <rifqizzz6@gmail.com>
2020-10-20 06:17:49 +0700

    fix blob download

--- resources/views/student/mini_assessment/exam/index.blade.php
+++ resources/views/student/mini_assessment/exam/index.blade.php
@@ -205,6 +205,26 @@ return is_array($value['answer']);
         doneCheckAnswer = false;
     })
 
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+
+        return "unknown";
+    }
+
     $('#download-answer').on('click', function() {
         // Function Agung in Here
         const urlPrint = "{!! URL::to('/student/mini_assessment/exam/pdf') !!}";
@@ -248,7 +268,7 @@ return is_array($value['answer']);
                     } else {
                         // For other browsers:
                         // Create a link pointing to the ObjectURL containing the blob.
-                        const data = window.URL.createObjectURL(newBlob);
+                        const data = window.URL.createObjectURL(blob);
                         var link = document.createElement('a');
                         link.href = data;
                         link.download="file.pdf";
