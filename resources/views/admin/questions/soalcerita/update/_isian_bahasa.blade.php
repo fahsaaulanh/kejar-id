6a5531075ea11faa84db93bf98c8a38b7d976e86
idsdeveloper <rifqizzz6@gmail.com>
2020-11-09 11:34:35 +0700

    rebase from master branch 328-curriculum-admin-soal-cerita-isian-bahasa

---
+++ resources/views/admin/questions/soalcerita/update/_isian_bahasa.blade.php
@@ -0,0 +1,82 @@
+<div class="modal fade modal-large" id="edit-isian-bahasa">
+    <div class="modal-dialog" role="document">
+        <form method="POST" novalidate>
+            @csrf
+            @method('PATCH')
+            <input type="hidden" name="question_type" value="QSAT">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Input Soal Isian Bahasa</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="kejar kejar-close"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group ck-height-9 ckeditor-list">
+                        <label>Soal</label>
+                        <textarea class="textarea-field ckeditor-field" name="question" placeholder="Ketik soal" required></textarea>
+                        <div class="ckeditor-btn-group ckeditor-btn-1 d-none">
+                            <button type="button" class="bold-btn" title="Bold (Ctrl + B)">
+                                <i class="kejar-bold"></i>
+                            </button>
+                            <button type="button" class="italic-btn" title="Italic (Ctrl + I)">
+                                <i class="kejar-italic"></i>
+                            </button>
+                            <button type="button" class="underline-btn" title="Underline (Ctrl + U)">
+                                <i class="kejar-underlined"></i>
+                            </button>
+                            <button type="button" class="bullet-list-btn" title="Bulleted list">
+                                <i class="kejar-bullet"></i>
+                            </button>
+                            <button type="button" class="number-list-btn" title="Number list">
+                                <i class="kejar-number"></i>
+                            </button>
+                            <button type="button" class="photo-btn" title="Masukkan foto">
+                                <i class="kejar-photo"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Jawaban</label>
+                        <p>Semua alternatif jawaban dianggap benar.</p>
+                        <table class="answer-list-table-ib" data-type="isian-bahasa">
+                        </table>
+                        <button class="btn btn-add border-0 pl-0 add-btn" type="button" data-type="isian-bahasa">
+                            <i class="kejar-add"></i> Tambah Alternatif Jawaban
+                        </button>
+                    </div>
+                    <div class="form-group ck-height-9 ckeditor-list">
+                        <label>Pembahasan</label>
+                        <textarea class="textarea-field ckeditor-field" name="explanation" placeholder="Ketik pembahasan"></textarea>
+                        <div class="ckeditor-btn-group ckeditor-btn-1 d-none">
+                            <button type="button" class="bold-btn" title="Bold (Ctrl + B)">
+                                <i class="kejar-bold"></i>
+                            </button>
+                            <button type="button" class="italic-btn" title="Italic (Ctrl + I)">
+                                <i class="kejar-italic"></i>
+                            </button>
+                            <button type="button" class="underline-btn" title="Underline (Ctrl + U)">
+                                <i class="kejar-underlined"></i>
+                            </button>
+                            <button type="button" class="bullet-list-btn" title="Bulleted list">
+                                <i class="kejar-bullet"></i>
+                            </button>
+                            <button type="button" class="number-list-btn" title="Number list">
+                                <i class="kejar-number"></i>
+                            </button>
+                            <button type="button" class="photo-btn" title="Masukkan foto">
+                                <i class="kejar-photo"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-end">
+                    <div>
+                        <button type="button" class="btn btn-cancel" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
